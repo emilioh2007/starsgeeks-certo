33d60f6a24dad2818b5decc83d296f8d8106b2f2
Emílo Holgado da Costa <emiliocosta@portalsesisp.org.br>
2023-05-16 08:25:34 -0300

    Logica Inicial

---
+++ cadastro.js
@@ -0,0 +1,59 @@
+const mensagem = document.querySelector(".mensagem");
+const formulario = document.getElementById("formulario");
+const nome = document.getElementById("mnome");
+const email = document.getElementById("memail");
+const senha = document.getElementById("psenha");
+const csenha = document.getElementById("csenha");
+const check = document.getElementById("taceitar");
+
+
+formulario.onsubmit = (evento) => {
+    if (nome.value == "") {
+        evento.preventDefault();
+        mensagem.innerHTML = "<p> Digite seu Nome! </p>";
+        nome.focus();
+        return null;
+    }
+
+    if (email.value == "") {
+        evento.preventDefault();
+        mensagem.innerHTML = "<p> Digite seu Email! </p>";
+        email.focus();
+        return null;
+    }
+
+    if (senha.value == "") {
+        evento.preventDefault();
+        mensagem.innerHTML = "<p> Digite sua Senha! </p>";
+        senha.focus();
+        return null;
+    }
+
+    if (csenha.value != senha.value) {
+        evento.preventDefault();
+        mensagem.innerHTML = "<p> Senhas precisam ser iguais! </p>";
+        csenha.focus();
+        return null;
+    }
+    if (check.value == "") {
+        evento.preventDefault();
+        mensagem.innerHTML = "<p> Necessario aceitar os termos! </p>";
+        email.focus();
+        return null;
+    }
+
+    let dados = JSON.parse(localStorage.getItem("dados")) || [];
+    dados.push({
+        nome : nome.value, 
+        email : email.value, 
+        senha : senha.value
+    })
+
+    localStorage.setItem("dados", JSON.stringify(dados));
+    evento.preventDefault();
+    mensagem.innerHTML = "<p> Parabens você se cadastrou com sucesso </p>"
+
+    setTimeout(() => {
+        window.location.assign("login.html");
+    }, 1000);
+}
---
+++ login.js
@@ -0,0 +1,23 @@
+const mensagem = document.querySelector(".mensagem");
+const nome = document.getElementById("mnome");
+const email = document.getElementById("memail");
+const senha = document.getElementById("psenha");
+const formulario = document.getElementById("formulario");
+
+
+formulario.onsubmit = (evento) => {
+    let dados = JSON.parse(localStorage.getItem("dados"));
+
+    dados.forEach((elemento) => {
+        if (elemento.email === email.value && elemento.senha === senha.value) {
+            evento.preventDefault();
+            window.location.assign("catalogo.html");
+            return true;
+        }
+        else{
+            evento.preventDefault();
+            mensagem.innerHTML="<p> Senha ou E-mail incorretos </p>";
+            
+        }
+    });
+}
